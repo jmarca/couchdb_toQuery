2685a64e8913c0905324f3ff2a7f3cd904391034
James E. Marca <jmarca@translab.its.uci.edu>
2013-04-08 14:03:46 -0700

    initial commit, copying toQuery from geom_utils

---
+++ README.md
@@ -0,0 +1,22 @@
+# CouchDB toQuery
+
+Tiny little utility that I use a lot to convert query objects into
+proper CouchDB query strings.  The deal is that you have to pass key,
+startkey, and endkey parameters as JSON, while everything else needs
+to be encoded as a string.  Also, long ago I stuffed in a simple
+little hack that makes sure that numbers stay numbers in this
+process.
+
+This is totally a use at your own risk type of module.  It works for
+me because this is how I work, but perhaps it won't work for you.
+
+# Acknowledgment
+
+The origins of this module are from way back when Felix G's
+node-couchdb module first went unmaintained.  I switched to just
+directly querying couchdb, and ran into problems.  I copied the
+'toQuery' string from there.
+
+Then later I kept getting hit with irritating cases in which numbers
+would get converted to text, which of course will sort and match
+completely differently, so I slapped in the brutal_hack.
---
+++ toquery.js
@@ -0,0 +1,31 @@
+/*global require JSON */
+var _ = require('lodash')
+var querystring = require('querystring')
+// make sure that strings are strings, numbers are numbers when JSON encoding
+function brutal_hack(v){
+    if(! isNaN(v) ){
+        return +v;
+    }else{
+        return v;
+    }
+}
+// Encode only key, startkey and endkey as JSON
+var toQuery = function(query) {
+        var q={};
+        _.each(query,function(v,k){
+            if (['key', 'keys', 'startkey', 'endkey'].indexOf(k) != -1) {
+                // keys can be arrays or strings
+                var vmap;
+                if(_.isArray(v)){
+                    vmap = _.map(v,function(value){ return brutal_hack(value); });
+                }else{
+                    vmap = brutal_hack(v);
+                }
+                q[k] = JSON.stringify(vmap);
+            } else {
+                q[k] = String(v);
+            }
+        });
+        return querystring.stringify(q);
+};
+module.exports=toQuery
