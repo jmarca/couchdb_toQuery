67149e94329f39ab16bde33fdb0a3ca700274ed6
James E. Marca <jmarca@translab.its.uci.edu>
2013-04-17 23:39:47 -0700

    added a test file

---
+++ test/test_toQuery.js
@@ -0,0 +1,13 @@
+var tq = require ('../.')
+var should = require('should')
+var query = {
+    'startkey':[10,'20','02']
+}
+
+describe('string parsing with numbers',function(){
+    it('should not get confused about 02',function(done){
+        var pq = tq(query)
+        pq.should.eql('startkey=%5B10%2C%2220%22%2C%2202%22%5D')
+        done()
+    })
+})
