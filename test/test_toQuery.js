8e17bdf6e0c0c762ab826e94cb105f44a0cc5318
James E. Marca <james@activimetrics.com>
2018-03-18 08:31:47 -0700

    switch from mocha to tap
    finally!

--- test/test_toQuery.js
+++ test/test_toQuery.js
@@ -1,21 +1,29 @@
+const tap = require('tap')
 var tq = require ('../.')
-var should = require('should')
 var query = {
     'startkey':[10,'20','02']
 }
 
-describe('string parsing with numbers',function(){
-    it('should not get confused about 02',function(done){
+function string_with_numbers(t){
+    t.plan(1)
+    return t.test('should not get confused about 02',tt=>{
+        tt.plan(1)
         var pq = tq(query)
-        pq.should.eql('startkey=%5B10%2C%2220%22%2C%2202%22%5D')
-        done()
+        tt.is(pq,'startkey=%5B10%2C%2220%22%2C%2202%22%5D')
+        return tt.end()
     })
-})
-describe('parse an object okay',function(){
-    it('should not turn 0 into a string',function(done){
+}
+
+function parse_an_object_okay(t){
+    t.plan(1)
+    return t.test('should not turn 0 into a string',tt=>{
+        tt.plan(1)
         var parsed = tq({limit:200
                         ,startkey:0})
-        parsed.should.eql('limit=200&startkey=0')
-        done()
+        tt.is(parsed,'limit=200&startkey=0')
+        return tt.end()
     })
-})
+}
+
+tap.test('string with numbers',string_with_numbers)
+tap.test('parse object',parse_an_object_okay)
--- toquery.js
+++ toquery.js
@@ -1,31 +1,32 @@
 /*global require JSON */
-var _ = require('lodash')
+var isString = require('lodash.isstring')
 var querystring = require('querystring')
+var forEach = require('lodash.foreach')
 // make sure that strings are strings, numbers are numbers when JSON encoding
 function brutal_hack(v){
-    if(! _.isString(v) && ! isNaN(v) ){
-        return +v;
+    if(! isString(v) && ! isNaN(v) ){
+        return +v
     }else{
-        return v;
+        return v
     }
 }
 // Encode only key, startkey and endkey as JSON
-var toQuery = function(query) {
-        var q={};
-        _.each(query,function(v,k){
-            if (['key', 'keys', 'startkey', 'endkey'].indexOf(k) != -1) {
-                // keys can be arrays or strings
-                var vmap;
-                if(_.isArray(v)){
-                    vmap = _.map(v,function(value){ return brutal_hack(value); });
-                }else{
-                    vmap = brutal_hack(v);
-                }
-                q[k] = JSON.stringify(vmap);
-            } else {
-                q[k] = String(v);
+function toQuery(query) {
+    var q={}
+    forEach(query,function(v,k){
+        if (['key', 'keys', 'startkey', 'endkey'].indexOf(k) != -1) {
+            // keys can be arrays or strings
+            var vmap;
+            if(Array.isArray(v)){
+                vmap = v.map(value => brutal_hack(value))
+            }else{
+                vmap = brutal_hack(v)
             }
-        });
-        return querystring.stringify(q);
-};
+            q[k] = JSON.stringify(vmap)
+        } else {
+            q[k] = String(v)
+        }
+    })
+    return querystring.stringify(q)
+}
 module.exports=toQuery
